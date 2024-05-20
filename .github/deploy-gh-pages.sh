9880194d7f1ac188382c1fa7a8215f174117eafb
Demetry Pascal <qtckpuhdsa@gmail.com>
2024-05-20 18:54:59 +0300

    -

--- .github/deploy-gh-pages.sh
+++ .github/deploy-gh-pages.sh
@@ -23,7 +23,7 @@ head=$(git rev-parse HEAD)
 
 git clone -b gh-pages "https://pasaopasen:$GH_PASSWORD@github.com/$GITHUB_REPOSITORY.git" gh-pages
 mkdir -p gh-pages/
-cp -R build/html/* gh-pages/
+cp -R build/html/* build/html/.* gh-pages/
 cd gh-pages
 sitemap
 git add *
