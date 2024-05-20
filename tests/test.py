19c51e9f0cff3efad7f4bcc4bb0654dc63cd10b3
demetry pascal <qtckpuhdsa@gmail.com>
2024-05-20 11:33:09 +0300

    refactor test.py

--- tests/test.py
+++ tests/test.py
@@ -3,19 +3,23 @@ import sys
 import unittest
 import ntpath
 
-file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.insert(0, os.path.abspath(file_path))
+from pathlib import Path
+
+PROJECT_PATH = Path(__file__).parents[1]
+
+sys.path.insert(0, str(PROJECT_PATH))
 
 import dashtable
+from dashtable.utils.files import read_text
 
 
 class TestMatches(unittest.TestCase):
     def setUp(self):
-        self.static_path = os.path.join(file_path, 'tests', 'static')
-
+        self.static_path = os.path.join(PROJECT_PATH, 'tests', 'static')
 
     def test_html_to_tables(self):
         for file in os.listdir(self.static_path):
+
             if file.endswith('.html'):
                 html_path = os.path.join(self.static_path, file)
                 rst = dashtable.html2rst(html_path)
@@ -23,9 +27,7 @@ class TestMatches(unittest.TestCase):
 
                 rst_name = os.path.splitext(file)[0] + '.rst'
                 rst_path = os.path.join(self.static_path, rst_name)
-                rst_file = open(rst_path, 'r', encoding='utf-8')
-                rst_text = rst_file.read().rstrip()
-                rst_file.close()
+                rst_text = read_text(rst_path).rstrip()
 
                 try:
                     self.assertEqual(rst, rst_text)
@@ -35,9 +37,7 @@ class TestMatches(unittest.TestCase):
 
                 md_name = os.path.splitext(file)[0] + '.md'
                 md_path = os.path.join(self.static_path, md_name)
-                md_file = open(md_path, 'r', encoding='utf-8')
-                md_text = md_file.read().rstrip()
-                md_file.close()
+                md_text = read_text(md_path).rstrip()
 
                 try:
                     self.assertEqual(md, md_text)
