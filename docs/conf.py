0c9c08871d6523cd36df1d3ed8cc2def8fa7f18f
Demetry Pascal <qtckpuhdsa@gmail.com>
2024-05-20 18:41:33 +0300

    -

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,11 @@ sys.path.insert(0, os.path.join(os.path.abspath('.'), '..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = [
+    'sphinx.ext.autodoc', 
+    'sphinx.ext.napoleon',
+    'sphinx.ext.githubpages'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
