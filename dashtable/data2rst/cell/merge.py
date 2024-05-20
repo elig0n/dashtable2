0bafb6a232c08590b7cc542ff2783b84eab3d283
demetry pascal <qtckpuhdsa@gmail.com>
2024-05-20 20:58:01 +0300

    fix wrong merge_cells TOP BOTTOM merge on hard cases

--- dashtable/data2rst/cell/merge.py
+++ dashtable/data2rst/cell/merge.py
@@ -8,6 +8,20 @@ from .cell import Cell
 MERGE_DIRECTION: TypeAlias = Optional[Literal["LEFT", "RIGHT", "BOTTOM", "TOP"]]
 
 
+def _merge_border_strings(s1: str, s2: str) -> str:
+    """
+    merges takes first string with + replaces from second string
+
+    >>> _merge_border_strings('+====+======+', '--+----------')
+    '+=+==+======+'
+    """
+    assert len(s1) == len(s2)
+    return ''.join(
+        '+' if '+' in (v1, v2) else v1
+        for v1, v2 in zip(s1, s2)
+    )
+
+
 def get_merge_direction(cell1: Cell, cell2: Cell) -> MERGE_DIRECTION:
     """
     Determine the side of cell1 that can be merged with cell2.
@@ -109,34 +123,43 @@ def merge_cells(cell1: Cell, cell2: Cell, direction: MERGE_DIRECTION):
 
     if direction == "RIGHT":
         for i in range(len(cell1_lines)):
-            cell1_lines[i] = cell1_lines[i] + cell2_lines[i][1::]
+            cell1_lines[i] = cell1_lines[i] + cell2_lines[i][1:]
         cell1.text = "\n".join(cell1_lines)
         cell1.column_count += cell2.column_count
 
     elif direction == "TOP":
-        if cell1_lines[0].count('+') > cell2_lines[-1].count('+'):
-            cell2_lines.pop(-1)
-        else:
-            cell1_lines.pop(0)
-        cell2_lines.extend(cell1_lines)
-        cell1.text = "\n".join(cell2_lines)
+
+        new_lines = cell2_lines[:-1] + [
+            _merge_border_strings(cell2_lines[-1], cell1_lines[0])
+        ] + cell1_lines[1:]
+
+        # if cell1_lines[0].count('+') > cell2_lines[-1].count('+'):
+        #     cell2_lines.pop(-1)
+        # else:
+        #     cell1_lines.pop(0)
+        # cell2_lines.extend(cell1_lines)
+
+        cell1.text = "\n".join(new_lines)
         cell1.row_count += cell2.row_count
         cell1.row = cell2.row
         cell1.column = cell2.column
 
     elif direction == "BOTTOM":
-        if (cell1_lines[-1].count('+') > cell2_lines[0].count('+') or
-                cell1.is_header):
-            cell2_lines.pop(0)
-        else:
-            cell1_lines.pop(-1)
-        cell1_lines.extend(cell2_lines)
-        cell1.text = "\n".join(cell1_lines)
+        # if cell1.is_header or cell1_lines[-1].count('+') > cell2_lines[0].count('+'):
+        #     cell2_lines.pop(0)
+        # else:
+        #     cell1_lines.pop(-1)
+        # cell1_lines.extend(cell2_lines)
+
+        new_lines = cell1_lines[:-1] + [
+            _merge_border_strings(cell1_lines[-1], cell2_lines[0])
+        ] + cell2_lines[1:]
+        cell1.text = "\n".join(new_lines)
         cell1.row_count += cell2.row_count
 
     elif direction == "LEFT":
         for i in range(len(cell1_lines)):
-            cell1_lines[i] = cell2_lines[i][0:-1] + cell1_lines[i]
+            cell1_lines[i] = cell2_lines[i][:-1] + cell1_lines[i]
         cell1.text = "\n".join(cell1_lines)
         cell1.column_count += cell2.column_count
         cell1.row = cell2.row
