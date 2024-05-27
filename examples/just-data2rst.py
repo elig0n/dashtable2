c9f788585408e491d31953de5e9c545959e382c6
demetry pascal <qtckpuhdsa@gmail.com>
2024-05-27 09:55:33 +0300

    -

---
+++ examples/just-data2rst.py
@@ -0,0 +1,36 @@
+
+
+
+import os
+import sys
+
+sys.path.append('../')
+
+from dashtable.utils.files import read_json, write_text
+from dashtable.exceptions import NonMergableException
+from dashtable.data2rst import data2rst
+
+
+def main():
+    spans = [
+        [[3, 1], [4, 1]],
+        [[3, 2], [4, 2]],
+        [[2, 1], [2, 2]],
+    ]
+    table = [
+        ["Header 1", "Header 2", "Header 3"],
+        ["body row 1", "column 2", "column 3"],
+        ["body row 2", "Cells may span columns.", ""],
+        ["body row 3", "Cells may\nspan rows.", "- Cells\n- contain\n- blocks."],
+        ["body row 4", "", ""],
+    ]
+
+    r = data2rst(table=table, spans=spans)
+
+    print(r)
+
+
+if __name__ == '__main__':
+    main()
+
+
